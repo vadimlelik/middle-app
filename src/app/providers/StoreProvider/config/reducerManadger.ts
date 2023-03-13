c2b0fd5b0d8e31765ed9f3ca626c1b73f123d751
mac <mac@MacBook-Pro-mac.local>
2023-03-13 20:29:43 +0300

    add Code Splitting

---
+++ src/app/providers/StoreProvider/config/reducerManadger.ts
@@ -0,0 +1,48 @@
+import { combineReducers, ReducersMapObject } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+
+export function createReducerManager(initialReducers: ReducersMapObject<StateSchema>) {
+    const reducers = { ...initialReducers };
+
+    let combinedReducer = combineReducers(reducers);
+
+    let keysToRemove = [];
+
+    return {
+        getReducerMap: () => reducers,
+
+        reduce: (state, action) => {
+            if (keysToRemove.length > 0) {
+                state = { ...state };
+                for (const key of keysToRemove) {
+                    delete state[key];
+                }
+                keysToRemove = [];
+            }
+
+            return combinedReducer(state, action);
+        },
+
+        add: (key, reducer) => {
+            if (!key || reducers[key]) {
+                return;
+            }
+
+            reducers[key] = reducer;
+
+            combinedReducer = combineReducers(reducers);
+        },
+
+        remove: (key) => {
+            if (!key || !reducers[key]) {
+                return;
+            }
+
+            delete reducers[key];
+
+            keysToRemove.push(key);
+
+            combinedReducer = combineReducers(reducers);
+        },
+    };
+}
---
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -0,0 +1,26 @@
+import { Modal } from 'shared/ui/Modal/Modal';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Suspense } from 'react';
+import { LoginFormAsync } from 'features/AuthByUsername/ui/LoginForm/LoginFormAsync';
+import { Loader } from 'shared/ui/Loader/Loader';
+
+interface LoginModalProps {
+    className?: string;
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
+    <Modal
+        className={classNames('', {}, [className])}
+        isOpen={isOpen}
+        onClose={onClose}
+        lazy
+    >
+
+        <Suspense fallback={<Loader />}>
+            <LoginFormAsync />
+        </Suspense>
+
+    </Modal>
+);
